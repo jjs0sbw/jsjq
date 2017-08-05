1ef71a936662d1852cab1ccc6cdff804c0df0146
Joseph Simpson <jjs0sbw@gmail.com>
2017-08-04 21:16:11 -0700

    Updating jsjq projects

--- wt_5_1/public_html/email_list.js
+++ wt_5_1/public_html/email_list.js
@@ -1,33 +1,33 @@
-// "use strict";
+"use strict";
 
 var $ = function(id) {
     return document.getElementById(id);
 };
 
-var joinList = function() {
+var joinList = function () {
     var emailAddress1 = $("email_address1").value;
     var emailAddress2 = $("email_address2").value;
     var firstName = $("first_name").value;
     var errorMessage = "";
 
     // validate the entries
-    if (emailAddress1 == "") {
+    if (emailAddress1 === "") {
     	errorMessage = "First email address entry required";
         $("email_address1").focus();
-    } else if (emailAddress2 == "") {
+    } else if (emailAddress2 === "") {
     	errorMessage = "Second email address entry required";
     	$("email_address2").focus();
-    } else if (emailAddress2 != emailAddress1) {
+    } else if (emailAddress2 !== emailAddress1) {
     	errorMessage = "Email address entries must match";
     	$("email_address2").focus();
-    } else if (firstName == "") {
+    } else if (firstName === "") {
     	errorMessage = "First name entry required";
     	$("first_name").focus();
     }
 
     // submit the form if all entries are valid
     // otherwise, display an error message
-    if (errorMessage = "") {
+    if (errorMessage === "") {
         $("email_form").submit(); 
     } else {
     	alert(errorMessage);
@@ -35,6 +35,6 @@ var joinList = function() {
 };
 
 window.onload = function() {
-    $("join_List").onclick = joinList;
+    $("join_list").onclick = joinList;
     $("email_address1").focus();
 };
